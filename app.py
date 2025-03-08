0c057fb74b9d1a4cf6f6a1442c33af536e9f0bcf
Nadia <nalexa3@clemson.edu>
2025-03-08 10:18:20 -0500

    Added text box

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def home():
+    submitted_text = None
+    if request.method == "POST":
+        submitted_text = request.form.get["text"]
     return render_template("index.html")
 
 if __name__ == "__main__":
