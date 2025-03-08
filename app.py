88f703eb36543df4d2b97a499ce7b57b4376eae2
Nadia <nalexa3@clemson.edu>
2025-03-08 11:05:18 -0500

    Fixed typo

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def home():
     submitted_text = None
     if request.method == "POST":
-        submitted_text = request.form.get["text"]
-    return render_template("index.html")
+        submitted_text = request.form.get("user_input")
+    return render_template("index.html", submitted_text = submitted_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
