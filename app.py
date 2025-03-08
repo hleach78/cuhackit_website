966d2edaee0219c4d963698fbe35f17e25096834
Nadia <nalexa3@clemson.edu>
2025-03-08 14:41:47 -0500

    Added AI in website

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import os
 import json
+from utils import callAPI
 
 app = Flask(__name__)
 data_file = "submitted_data.json"
@@ -23,13 +24,20 @@ def collect_data(name, courses):
 def home():
     submitted_name = None
     submitted_courses = None
+    recommendation = None
     if request.method == "POST":
         submitted_name = request.form.get("user_name")
         submitted_courses = request.form.get("user_classes")
         if submitted_name and submitted_courses:
             collect_data(submitted_name, submitted_courses)
+            student = {
+                "name": submitted_name,
+                "completed_courses": [],
+                "current_courses": submitted_courses.split(",")
+            }
+            recommendation = callAPI(student)
     return render_template("index.html", submitted_name=submitted_name,
-            submitted_courses=submitted_courses)
+            submitted_courses=submitted_courses, recommendation=recommendation)
 
 if __name__ == "__main__":
     app.run(debug=True)
